bdb00b63cd4dc7a714e4c51493b0a9b000f544fd
excelsior091224 <excelsior091224@gmail.com>
2022-12-28 10:37:28 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,18 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: "server",
   //adapter: cloudflare({ mode: "directory" }),
   adapter: cloudflare(),
+  vite: {
+    define: {
+        "import.meta.env.MICROCMS_SERVICE_DOMAIN": import.meta.env.MICROCMS_SERVICE_DOMAIN,
+        "import.meta.env.MICROCMS_API_KEY":import.meta.env.MICROCMS_API_KEY,
+    },
+  },
 });
