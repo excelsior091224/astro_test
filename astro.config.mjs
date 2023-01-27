028c52a470aacef135e1901dab1995da61121ac0
excelsior091224 <excelsior091224@gmail.com>
2023-01-27 23:26:57 +0900

    updated astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  // output: "server",
-  //adapter: cloudflare({ mode: "directory" }),
-  // adapter: cloudflare(),
-  // vite: {
-  //   define: {
-  //       "process.env.MICROCMS_SERVICE_DOMAIN": process.env.MICROCMS_SERVICE_DOMAIN,
-  //       "process.env.MICROCMS_API_KEY":process.env.MICROCMS_API_KEY,
-  //   },
-  // },
 });
